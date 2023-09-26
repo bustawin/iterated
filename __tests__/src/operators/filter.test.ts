ffd5e7a47129c94493e73dec4563bc9fb90af1bc
bustawin <xavier@bustawin.com>
2023-09-26 20:09:39 +0200

    Update filter tests

--- __tests__/src/operators/filter.test.ts
+++ __tests__/src/operators/filter.test.ts
@@ -1,18 +1,56 @@
 import it from '@src'
+import exp = require('node:constants')
 
 describe('filter', () => {
-  test('filters based on a function', () => {
-    const result = it.filter(it.range(6), (num) => num > 3)
-    expect(it.array(result)).toEqual([4, 5])
-  })
+  describe('When passing a sync iterable', () => {
+    test('filters based on a function', () => {
+      const result = it.filter(it.range(6), (num) => num > 3)
+      expect(it.array(result)).toEqual([4, 5])
+    })
 
-  test('filters based on a value', () => {
-    const result = it.filter([1, 1, 2, 3, 4, 5, 4], 4)
-    expect(it.array(result)).toEqual([4, 4])
-  })
+    test('filters based on a value', () => {
+      const result = it.filter([1, 1, 2, 3, 4, 5, 4], 4)
+      expect(it.array(result)).toEqual([4, 4])
+    })
 
-  test('returns an empty iterator when nothing matches', () => {
-    const result = it.filter(it.range(2), 9)
-    expect(it.array(result)).toEqual([])
+    test('returns an empty iterator when nothing matches', () => {
+      const result = it.filter(it.range(2), 9)
+      expect(it.array(result)).toEqual([])
+    })
+    test('returns empty when passed empty', () => {
+      const result = it.pipe('', it.filter.p('9'), it.array)
+      expect(result).toEqual([])
+    })
+  })
+  describe('When passing an async iterable', () => {
+    test('filters based on a function', async () => {
+      const result = await it.pipe(
+        it.range(6),
+        it.async,
+        it.filter.p((num) => num > 3),
+        it.array,
+      )
+      expect(result).toEqual([4, 5])
+    })
+
+    test('filters based on a value', async () => {
+      const result = await it.pipe(
+        [1, 1, 2, 3, 4, 5, 4],
+        it.async,
+        it.filter.p(4),
+        it.array,
+      )
+      expect(result).toEqual([4, 4])
+    })
+
+    test('returns an empty iterator when nothing matches', async () => {
+      const result = await it.pipe(it.range(2), it.async, it.filter.p(9), it.array)
+      expect(result).toEqual([])
+    })
+
+    test('returns empty when passed empty', async () => {
+      const result = await it.pipe('', it.async, it.filter.p('9'), it.array)
+      expect(result).toEqual([])
+    })
   })
 })
