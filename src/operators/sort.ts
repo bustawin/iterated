c9cd505ee1a1a09d1812324ec2acdc8aa5d61c18
bustawin <xavier@bustawin.com>
2024-04-03 07:25:57 +0200

    Add async sort

--- src/operators/sort.ts
+++ src/operators/sort.ts
@@ -1,11 +1,27 @@
-import { It } from '../base'
+import { AIt, AnyIt, AnyItV, It } from '@src'
 import { toPipe } from '../pipe'
+import { chooseFunc } from '@src/iterators'
+import array from '@src/array'
 
-export function sort<IterValue>(
-  iter: It<IterValue>,
-  comparator: (a: IterValue, b: IterValue) => -1 | 0 | 1,
-): IterValue[] {
-  return [...iter].sort(comparator)
+export type comparator = -1 | 0 | 1
+
+export function sort<Iter extends AnyIt<V>, V = AnyItV<Iter>>(
+  iter: Iter,
+  comparator: (a: V, b: V) => comparator,
+) {
+  return chooseFunc(iter, _sort, _aSort, comparator)
 }
 
 sort.p = toPipe(sort)
+
+async function _aSort<V>(
+  iter: AIt<V>,
+  comparator: (a: V, b: V) => comparator,
+): Promise<V[]> {
+  const arr = await array(iter)
+  return arr.sort(comparator)
+}
+
+function _sort<V>(iter: It<V>, comparator: (a: V, b: V) => comparator): V[] {
+  return [...iter].sort(comparator)
+}
