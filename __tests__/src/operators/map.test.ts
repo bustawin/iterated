3d9b9646ef723f01a5aa71a446b02c8c733ffc21
bustawin <xavier@bustawin.com>
2024-06-09 18:05:19 +0200

    Add map tests

--- __tests__/src/operators/map.test.ts
+++ __tests__/src/operators/map.test.ts
@@ -1,13 +1,22 @@
 import it from '@src'
 
 describe('map', () => {
-  test('maps', () => {
-    const result = it.map(it.range(3), (num) => num * 2)
-    expect(it.array(result)).toEqual([0, 2, 4])
+  describe('When passing a sync iterable', () => {
+    test('applies the function to every element of the iterable', () => {
+      const result = it.map(it.range(3), (num) => num * 2)
+      expect(it.array(result)).toEqual([0, 2, 4])
+    })
+
+    test('Return empty when passed empty', () => {
+      const result = it.map([], it.identity)
+      expect(it.array(result)).toEqual([])
+    })
   })
 
-  test('maps async', async () => {
-    const result = it.map(it.async(it.range(3)), (num) => num * 2)
-    await expect(it.array(result)).resolves.toEqual([0, 2, 4])
+  describe('When passing an async iterable', () => {
+    test('maps async', async () => {
+      const result = it.map(it.async(it.range(3)), (num) => num * 2)
+      await expect(it.array(result)).resolves.toEqual([0, 2, 4])
+    })
   })
 })
